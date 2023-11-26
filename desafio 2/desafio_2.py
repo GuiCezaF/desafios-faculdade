163cfcf18e8b9702a791c473f38e5dc3acb16d12
GuiCezaF <contato.guicezafe@gmail.com>
2023-11-25 23:27:53 -0300

    desafio 2 concluído

---
+++ desafio 2/desafio_2.py
@@ -0,0 +1,18 @@
+# Escreva um algoritmo que pegue um array e mova todos os zeros para o final, preservando a ordem dos outros elementos
+
+
+def move_zeros(arr):
+    zeros = []
+
+    for num in arr:
+        if num != 0:
+            zeros.append(num)
+
+    zeros.extend([0] * (len(arr) - len(zeros)))
+
+    return zeros
+
+
+array = [0, 5, 31, 0, 9, 0, 0, 10, 50]
+resultado = move_zeros(array)
+print(resultado)
